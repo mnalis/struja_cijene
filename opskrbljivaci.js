193d6bafe08e25a27fc9a5335e6b3f33c7bbd9ee
Matija Nalis <mnalis-git@voyager.hr>
2018-11-23 19:22:34 +0100

    initial table filling example

---
+++ opskrbljivaci.js
@@ -0,0 +1,16 @@
+//
+// lista opskrbljivaca sa cijenama i popustima
+//
+
+var last_updated='2018-11-23';
+
+var opskrbljivaci = { 'cfg': [
+{       'naziv': 'HEP Univerzalna usluga (default)',
+	'jt_cijena': 0.77,
+	'vt_cijena': 0.84,
+	'nt_cijena': 0.41,
+	'naknada_omm': 10,
+	'naknada_opskrba': 7.40,
+},
+
+] };
---
+++ struja_calc.js
@@ -0,0 +1,37 @@
+function loadTable() {
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions_and_function_scope/Strict_mode
+	'use strict';
+
+	document.getElementById('tablica').innerHTML = '<tr><td colspan=5>Initializing...</td></tr>';
+
+//  document.getElementById('expand_op').onchange = check_expand;
+//  check_expand();
+
+	// dodaj jedan red za jednog opskrbljivaca
+	function addRow(op) {
+		var row='<tr>' + 
+			'<td>' + op.naziv +  '</td>' +
+			'<td>' + '123' +  '</td>' +
+			'<td>' + '234' +  '</td>' +
+			'<td>' + '567' +  '</td>' +
+			'<td>' + '88' +  '</td>' +
+			'</tr>';
+		return row;
+	}
+
+	var tablica = '';
+	var i = 0;
+	while (i < opskrbljivaci.cfg.length) {
+		tablica = tablica + addRow(opskrbljivaci.cfg[i++]);
+	};
+
+
+
+
+
+	document.getElementById('tablica').innerHTML = tablica;
+	document.getElementById('zadnji_update').innerHTML = last_updated;
+
+}
+
+loadTable();
