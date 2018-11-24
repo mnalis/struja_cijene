6ac209bda0e82fc7f597882f8a9a40fcf34541f3
Matija Nalis <mnalis-git@voyager.hr>
2018-11-24 01:43:26 +0100

    initial calc

--- struja_calc.js
+++ struja_calc.js
@@ -12,35 +12,45 @@ function loadTable() {
 
 	// popunjavanje tablice
 	function recalc_table() {
-		// dodaj jedan red za jednog opskrbljivaca
-		function addRow(op) {
-			var calc_energija = 123;	// FIXME all hadcoded for test
-			var calc_mrezarina = 456;
-			var calc_total = 789;
-			var calc_usteda = 90;
-
-			var row='<tr>' + 
-				'<td>' + op.naziv +	  '</td>' +
-				'<td>' + calc_energija +  '</td>' +
-				'<td>' + calc_mrezarina + '</td>' +
-				'<td>' + calc_total +	  '</td>' +
-				'<td>' + calc_usteda +	  '</td>' +
-				'</tr>';
-			return row;
-		}
-
 		var period='godišnje';
+		var mjeseci=12;
 		if (document.getElementById('period').value == 'month') {
 			period='mjesečno';
+			mjeseci=1;
 		}
+
 		document.getElementById('h_energija').innerHTML  = 'Energija ' + period + ' [kn]';
 		document.getElementById('h_mrezarina').innerHTML = 'Mrežarina ' + period + ' [kn]';
 		document.getElementById('h_total').innerHTML = 'Ukupno ' + period + ' [kn]';
 		document.getElementById('h_usteda').innerHTML = 'Ušteda ' + period + ' [kn]';;
 
-
 		document.getElementById('l_vt').childNodes[0].nodeValue='Potrošnja ' + period + ' VT [kWh]';	// modifying .innerText also nukes <input> field in FF60 :(
 		document.getElementById('l_nt').childNodes[0].nodeValue='Potrošnja ' + period + ' NT [kWh]';
+
+		var vt_kwh = +document.getElementById('vt_kwh').value;		// convert string to number, otherwise vt_kwh+nt_kwh becomes a huge number!
+		var nt_kwh = +document.getElementById('nt_kwh').value;
+
+		var total_univerzalna = 'UNDEF';
+		// dodaj jedan red za jednog opskrbljivaca
+		function addRow(op) {
+			var calc_energija  = vt_kwh * op.kwh_vt_cijena     + nt_kwh * op.kwh_nt_cijena;
+			var calc_mrezarina = vt_kwh * op.kwh_ods_vt_cijena + nt_kwh * op.kwh_ods_nt_cijena + mjeseci * op.mj_naknada_omm;
+			var calc_razno     = (vt_kwh + nt_kwh) * (op.kwh_solidarna + op.kwh_oieik) - op.mj_popust + mjeseci * op.mj_naknada_opskrba;
+			var calc_osnovica  = calc_energija + calc_mrezarina + calc_razno;
+			var calc_porez	   = calc_osnovica * op.pct_pdv / 100;
+			var calc_total	   = calc_osnovica + calc_porez;
+			if (total_univerzalna == 'UNDEF') { total_univerzalna = calc_total; }
+			var calc_usteda    = total_univerzalna - calc_total;
+
+			var row='<tr>' + 
+				'<td>' + op.naziv			+ '</td>' +
+				'<td>' + calc_energija.toFixed(2)	+ '</td>' +
+				'<td>' + calc_mrezarina.toFixed(2)	+ '</td>' +
+				'<td>' + calc_total.toFixed(2)		+ '</td>' +
+				'<td>' + calc_usteda.toFixed(2)		+ '</td>' +
+				'</tr>';
+			return row;
+		}
 
 		var tablica = '';
 		var i = 0;
