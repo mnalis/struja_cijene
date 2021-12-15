89d0a41ffb768f1ead97f6742d82cebac758119a
Matija Nalis <mnalis-git@voyager.hr>
2021-12-16 00:26:36 +0100

    publish: better rounding precision

--- opskrbljivaci-04307e24f.js
+++ opskrbljivaci-04307e24f.js
@@ -2,7 +2,7 @@
 // lista opskrbljivaca sa cijenama i popustima
 //
 
-var last_updated='2021-12-15';	// autoupdated by Makefile
+var last_updated='2021-12-16';	// autoupdated by Makefile
 
 var op_defaults = {
 	'ima_mj_trosak_uplate': 1,	// odabir samog iznosa npr. PBZ naknada internet bankarstvo 2018/11 = 2.25kn/mj) u index.html
